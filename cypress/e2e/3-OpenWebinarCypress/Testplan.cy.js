caa84eb7cbcdc2f09dcf6606fd276e296f2bdf00
Isabel Torres <isabel0729.it@gmail.com>
2024-07-09 14:56:43 +0200

    ordenacions de A-Z y Z-A

--- cypress/e2e/3-OpenWebinarCypress/Testplan.cy.js
+++ cypress/e2e/3-OpenWebinarCypress/Testplan.cy.js
@@ -4,7 +4,7 @@ describe("Bateria de pruebas", () => {
     cy.visit("https://www.saucedemo.com")
   })
 
-  context("Login", () => {
+  context.skip("Login", () => {
 
     it("Login correcto", () => {
       cy.fixture("usuarios").then(usuarios => {
@@ -24,25 +24,29 @@ describe("Bateria de pruebas", () => {
 
   })
 
-  context("Ordenacion de catalogo", () => {
-
+  context("ordenacion de catalogo", () => {
+    //Antes de cualquier ordenacion hay que hacer un LOGIN correcto.
+    // Con un beforeEach.( cada vez que ejecutemos test de ordenacion necesitamos logearnos)
     beforeEach(() => {
       cy.fixture("usuarios").then(usuarios => {
         cy.loginSaucedemo(usuarios.usuario_correcto.usuario, usuarios.usuario_correcto.password)
       })
-      cy.get(".title").as("titulo")
-      cy.get("@titulo").should('have.text', "Products")
     })
-
     it("Comprobar ordenacion A-Z", () => {
+      // recupero la clase inventory list y luego dentro ( within) accedo a los otros elementos como el boton
+      // recuperamos el boton que esta dentro de la clase inventory list, y  recorremos  cada uno de los botones
+      // Por mostrar otra forma, nos ubicamos en el boton y luego vamos al padre del boton y luego entramos con find a sus elementos
+      // Invocamos al TEXTO, que tienen la etiqueta capturada del dom. Y la etiqueta devuelve el nombre del producto ( then $nombre) y lo presentamos por pantalla con el console log
+      // Luego de mostrarlos por pantalla se podran manipularlos
       cy.get(".inventory_list").within(() => {
         cy.get("button").each(($boton, index, $listado) => {
-          cy.wrap($boton)
+          cy.wrap($boton) // envuelve al boton
             .parents(".inventory_item_description")
-            .find(".inventory_item_name")
+            .find(".inventory_item_name ")
             .invoke('text').then($nombre => {
+              //console.log($nombre); ( esta guardado ahora en catalogo.json)
               cy.fixture("catalogo").then(catalogo => {
-                expect($nombre).to.equal(catalogo.productos[index])
+                expect($nombre).to.equal(catalogo.productos[index])// comparara el orden haciendo uso del index. Index inicia  en la posicion 0
               })
             })
         })
@@ -51,69 +55,29 @@ describe("Bateria de pruebas", () => {
 
     it("Comprobar ordenacion Z-A", () => {
 
-      cy.get("select[data-test='product_sort_container']").select("za")
+      cy.get("select[data-test='product-sort-container']").select("za")
 
       cy.get(".inventory_list").within(() => {
         cy.get("button").each(($boton, index, $listado) => {
           cy.wrap($boton)
             .parents(".inventory_item_description")
-            .find(".inventory_item_name")
+            .find(".inventory_item_name ")
             .invoke('text').then($nombre => {
               cy.fixture("catalogo").then(catalogo => {
                 expect($nombre).to.equal(catalogo.productos[catalogo.productos.length - index - 1])
+                //Orden a la inversa. Haremos lo contrario
+                // catalogo.products.length nos devuelve longitud del array
+                // - index por que partimos de cero(0)
+                // -1 Por que acudimos al ultimo indice ( en este caso el ultimo inidice es 5 - catalogo.json)
+                //El 1er index es siempre 0.
+                //Length = 6  ya que hay 6 productos en el fixture catalogo.json.
+                //6-0-1 = 5 (  posicion 5)
+                //Sgte iteracion: index ahora vale 1 -> 6 de length -1 - 1  = 4 ( posicion 4)
               })
             })
         })
       })
     })
-  })
-
-  context("Compra de productos", () => {
-
-    let productosAgregados;
-
-    beforeEach(() => {
-      cy.fixture("usuarios").then(usuarios => {
-        cy.loginSaucedemo(usuarios.usuario_correcto.usuario, usuarios.usuario_correcto.password)
-      })
-      cy.get(".title").as("titulo")
-      cy.get("@titulo").should('have.text', "Products")
-      productosAgregados = 0;
-    })
-
-    it("Comprobar carrito vacio al entrar", () => {
-      cy.get(".shopping_cart_link").click();
-      cy.get("@titulo").should('have.text', "Your Cart")
-      cy.get(".cart_list .cart_item").should('not.exist')
-    })
-
-    it("Comprar un producto", () => {
-      cy.get(".inventory_list").find("button").eq(1).click()
-
-      cy.get(".shopping_cart_link").click();
-      cy.get("@titulo").should('have.text', "Your Cart")
-
-      cy.checkout("compraUno")
-
-    })
-
-    it("Comprar todos los productos", () => {
-
-      cy.get(".inventory_list button").each($button => {
-        cy.wrap($button).click();
-        productosAgregados++;
-      })
-
-      cy.get(".shopping_cart_link").click();
-      cy.get("@titulo").should('have.text', "Your Cart")
-
-      cy.get(".cart_list .cart_item").then($elementos => {
-        expect($elementos).to.length(productosAgregados)
-      })
-
-      cy.checkout("compraTodo")
-    })
-
 
   })
 
